04288f0ce3824eb64820da588b5423cb5d912df6
Marius Willekes <willekesmarius@gmail.com>
2023-08-18 08:41:11 +0200

    watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Movie.destroy_all
+List.destroy_all
+
+puts 'Creating movies...'
+10.times do
+  movie = Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: rand(4.0...10.0).round(1))
+  puts "Created #{movie.title}"
+end
+
+puts 'Creating lists...'
+List.create(name: "Classic")
+List.create(name: "Drama")
+List.create(name: "Thriller")
+List.create(name: "Comedy")
+List.create(name: "Action")
+
+puts 'Finished!'
